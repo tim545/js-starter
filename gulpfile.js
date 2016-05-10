f2a7cc6193c7c9819ffefcbfb88e11e29126da11
Tim <tim.freedman@live.com.au>
2016-05-10 10:19:54 +1000

    completing the build process with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,73 @@
 
-var gulp = require('gulp');
-var gulpUtil = require('gulp-util');
-var path = require('path');
-var del = require('del');
+const gulp = require('gulp');
+const gulpUtil = require('gulp-util');
+const path = require('path');
+const del = require('del');
 
-var plugins = {
+const plugins = {
   jade: require('gulp-jade'),
   sass: require('gulp-sass'),
   babel: require('gulp-babel'),
   eslint: require('gulp-eslint'),
   server: require('gulp-server-livereload'),
-  webpack: require('webpack-stream'),
   rename: require('gulp-rename'),
   concat: require('gulp-concat'),
   minify: require('gulp-minify')
 };
 
-gulp.task('clean', function () {
-  del(['dist/**/*']);
+gulp.task('clean', ()=> {
+  return del(['dist/**/*']);
 });
 
-gulp.task('jade', function() {
-  gulp.src('src/index.jade')
+gulp.task('jade', ()=> {
+  return gulp.src('src/index.jade')
   .pipe(plugins.jade())
   .pipe(gulp.dest('dist'));
 });
 
-gulp.task('sass', function() {
-  gulp.src('src/assets/styles/style.scss')
+gulp.task('sass', ()=> {
+  return gulp.src('src/assets/styles/style.scss')
   .pipe(plugins.sass())
   .pipe(gulp.dest('dist'));
 });
 
-gulp.task('jsDependencies', function() {
-  gulp.src('src/dependencies/*.js')
+gulp.task('jsDependencies', ()=> {
+  return gulp.src('src/dependencies/*.js')
   .pipe(plugins.concat('app-dependencies.js'))
   .pipe(gulp.dest('dist'));
 });
 
-gulp.task('lint', function() {
-  gulp.src(['src/index.js', 'src/components/**/*.js', 'src/state/**/*.js'])
+gulp.task('lint', ()=> {
+  return gulp.src(['src/index.js', 'src/components/**/*.js', 'src/state/**/*.js'])
   .pipe(plugins.eslint())
   .pipe(plugins.eslint.format())
   .pipe(plugins.eslint.failAfterError());
 });
 
-gulp.task('babel', ['lint'], function() {
-  gulp.src(['src/index.js', 'src/components/**/*.js', 'src/state/**/*.js'])
+gulp.task('babel', ['lint'], ()=> {
+  return gulp.src(['src/index.js', 'src/components/**/*.js', 'src/state/**/*.js'])
   .pipe(plugins.babel())
   .pipe(plugins.concat('app.js'))
   .pipe(gulp.dest('dist'));
 });
 
-gulp.task('minify', ['babel'], function() {
-  gulp.src('dist/app.js')
+gulp.task('minify', ['babel'], ()=> {
+  return gulp.src(['dist/app.js'])
   .pipe(plugins.minify({mangle:false}))
   .pipe(gulp.dest('dist'));
 });
 
-gulp.task('server', function() {
-  gulp
-  .src(path.join(__dirname, 'dist'))
+gulp.task('server', ['minify'], ()=> {
+  return gulp.src(path.join(__dirname, 'dist'))
   .pipe(plugins.server({
     livereload: true,
-    open: true
+    open: true,
+    log: 'debug'
   }));
 });
 
-gulp.task('default', ['clean', 'jade', 'sass', 'jsDependencies', 'lint', 'babel', 'minify', 'server'], function() {
-  gulpUtil.log('Complete Gulp compile without errors');
-});
+gulp.task('start', ['clean', 'jade', 'sass', 'jsDependencies', 'lint', 'babel', 'minify', 'server']);
+gulp.task('update', ['jade', 'sass', 'lint', 'babel', 'minify'])
 
-gulp.watch('src/**.*', ['default']);
+gulp.task('default', ['start']);
+gulp.watch('src/**.*', ['update']);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 
-// const store = createReduxStore();
-const Conponent = React.Component;
+const Component = React.Component;
 const render = ReactDOM.render;
 const appContiner = document.getElementById('app');
 
 class App extends Component {
   render() {
     return (
-      <App>
-        <p>This is the app.</p>
-      </App>
+      <p>This is the app.</p>
     );
   }
 }
 
-render(App, appContiner);
+render(<App />, appContiner);
